8e77ce38f4b9520e74ce1e0ee55d068f13b9d48f
Johnny Hoang <hoang.joh@gmail.com>
2018-01-31 15:53:53 +0100

    portail test
    commit du portail

--- Assets/Circle.cs
+++ Assets/Circle.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Circle : MonoBehaviour {
-    private float velocity =3.0f;
+    private float velocity =0.8f;
     void Update()
     {
         transform.Translate(velocity*0.05f *Vector3.right * Time.deltaTime);
